40fec151e29534046a7b5c3836ad0fdf612ea71d
PosikBoy <z.kalinichenko4@gmail.com>
2025-03-26 01:12:20 +0300

    redis module

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,4 +1,4 @@
-import { Global, Module } from "@nestjs/common";
+import { Global, Logger, Module } from "@nestjs/common";
 import Redis from "ioredis";
 import { RedisService } from "./redis.service";
 
@@ -8,10 +8,30 @@ import { RedisService } from "./redis.service";
     {
       provide: "REDIS_CLIENT",
       useFactory: () => {
-        return new Redis({
+        const logger = new Logger("RedisModule");
+
+        const redis = new Redis({
           host: process.env.REDIS_HOST,
           port: +process.env.REDIS_PORT,
+          retryStrategy: (times) => {
+            const delay = Math.min(times * 50, 2000);
+            return delay;
+          },
         });
+
+        redis.on("connect", () => {
+          logger.log("Connected to Redis");
+        });
+
+        redis.on("error", (err) => {
+          logger.error("Redis error", err);
+        });
+
+        redis.on("reconnecting", () => {
+          logger.warn("Reconnecting to Redis");
+        });
+
+        return redis;
       },
     },
     RedisService,
